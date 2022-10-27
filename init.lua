5be66bfe9b696397aa735fb413c9fb6d17ec70fd
chmodsayshello <chmodsayshello@hotmail.com>
2022-10-27 20:57:02 +0200

    minor changes (https://content.minetest.net/threads/3550/)

--- init.lua
+++ init.lua
@@ -1,22 +1,17 @@
--- register extra flavours of a base nodedef
-local timer = 0
+-- register extra flavours of a base 
+-- comment by chmodsayshello: didn't change name "on_walk_over", since that would break compability with prior versions
 minetest.register_globalstep(function(dtime)
-	timer = timer + dtime;
-	if timer >= 1 then
-		for _,player in pairs(minetest.get_connected_players()) do
-            local loc = vector.add(player:getpos(),{x=0,y=-1,z=0})
-            if loc ~= nil then
-               
-                local nodeiamon = minetest.get_node(loc)
-                if nodeiamon ~= nil then
-                    local def = minetest.registered_nodes[nodeiamon.name]
-                    if def ~= nil and def.on_walk_over ~= nil then
-                        def.on_walk_over(loc, nodeiamon, player)
-                    end
-                end   
-            end
-        end
-	 
-		timer = 0
+	for _,player in pairs(minetest.get_connected_players()) do
+		--local loc = vector.add(player:getpos(),{x=0,y=-1,z=0}) --old implementation
+		local loc = vector.offset(player:getpos,0,-0.1,0)
+		if loc then
+			local nodeiamon = minetest.get_node(loc)
+			if nodeiamon then
+				local def = minetest.registered_nodes[nodeiamon.name]
+				if def and def.on_walk_over then
+				def.on_walk_over(loc, nodeiamon, player)
+				end
+			end   
+		end
 	end
 end)
